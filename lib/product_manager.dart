cd37d136a8ed1d9c931534a13d7ae27c444c51fc
Maunda <alex.maunda@rafikibet.com>
2018-12-10 08:43:27 +0300

    Finished the basics

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
+
   @override
   State<StatefulWidget> createState() {
     return _ProductManagerState();
@@ -10,7 +15,13 @@ class ProductManager extends StatefulWidget {
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Test'];
+
+  void initState() { 
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  List<String> _products = [];
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +30,7 @@ class _ProductManagerState extends State<ProductManager> {
         Container(
           margin: EdgeInsets.all(10.0),
           child: RaisedButton(
+            color: Theme.of(context).primaryColor,
             onPressed: () {
               setState(() {
                 _products.add('Advanced food tester');
